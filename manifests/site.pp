0d7b32123331bdae8089db6d150e0153a4c5abcb
Matt Coffey <matt.coffey.1983@gmail.com>
2013-10-08 10:58:33 +0100

    Added example of downloading and installing an RPM from HTTP
    Change-Id: Ie4562127c8bc868064b613e14e9a6680c2868b68

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,3 +7,20 @@ File { owner => vagrant, group => vagrant, mode => 0644 }
 file { '/home/vagrant/.bashrc':
     content => "echo 'May you never need to Stand One Up By Hand again!'"
 }
+
+package { "vim-enhanced":
+    ensure => present,
+}
+
+class jre ( $src ) {
+    package { 'package':
+        provider => 'rpm',
+        ensure => installed,
+        name => "jre-1.7.0_40-fcs.i586",
+        source => "${jre::src}"
+    }
+}
+
+class { 'jre':
+    src => 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=80802',
+}
